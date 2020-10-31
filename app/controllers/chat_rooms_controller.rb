770df439aba36ef5e24f55bd6563c4f75f3a3709
EC2 Default User <ec2-user@ip-172-31-32-113.us-west-2.compute.internal>
2020-10-31 13:29:37 +0000

    chat_room仮+# @commentをcontrollers/users/chat_roomsに追加

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -4,6 +4,8 @@ class ChatRoomsController < ApplicationController
     # @user = current_user.users
     @user = User.all
     @task = Task.find(params[:task_id])
+    # @comment = current_user.comments.build(comment_params)
+    # @comment.save
     # if @task.save
     #   redirect_to task_chat_rooms_path(@task)
     # end
