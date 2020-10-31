101ab590296c1cc3a383e74d983a2e7cef0f8884
EC2 Default User <ec2-user@ip-172-31-32-113.us-west-2.compute.internal>
2020-10-31 13:22:27 +0000

    chat_room仮

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,7 +1,12 @@
 class ChatRoomsController < ApplicationController
 
   def index
+    # @user = current_user.users
+    @user = User.all
     @task = Task.find(params[:task_id])
+    # if @task.save
+    #   redirect_to task_chat_rooms_path(@task)
+    # end
     render layout: false
   end
 end
