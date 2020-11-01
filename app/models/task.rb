3d374f8cef8c98554e5e650d40ba7824d0ea1ef5
EC2 Default User <ec2-user@ip-172-31-32-113.us-west-2.compute.internal>
2020-11-01 05:55:56 +0000

    chat_roomページコード確認用

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
   has_many :work_schedules
-  has_many :comment
+  has_many :comments
   has_one_attached :image
 
   enum status: { todo: 0, doing: 1, done: 2 }
