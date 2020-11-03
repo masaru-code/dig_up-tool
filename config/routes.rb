3233a0fa792e903194aaf1fe101cdfbd0aef27bc
EC2 Default User <ec2-user@ip-172-31-32-113.us-west-2.compute.internal>
2020-11-03 02:26:27 +0000

    4章sin_up Loginのスタイル変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,10 @@
 Rails.application.routes.draw do
 
   get 'chat_rooms/index'
-  #自分で書いたルート
-  # get 'dig_home/dighome'
-  # get 'dig_home/new'
-  # get 'dig_home/jikou_end'
-  # get 'dig_home/chat_room'
-  # get 'dig_home/minna'
-  # get 'dig_home/proces'
-  # get 'dig_home/dig_index' 
-  # devise_for :users
-  # resources :users
-  # resources :jikous
-  # root 'dig_top#home'  
-  # get 'users/index'
-  # get 'users/show'
-  # get 'dig_top/help'
-  # get 'dig_top/setumei'
-  # #root 'application#hello'
-  
   #講師に直してもらったroot
   # rails routesで詳細確認
-  root 'dig_top#home'
+  # root 'dig_top#home'
+  root to: "home#index"  
 
   get 'dig_home/dighome'
   get 'dig_home/jikou'
