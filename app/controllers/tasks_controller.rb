09e6779c5c893fc5de5035f32f680082e6fc488b
EC2 Default User <ec2-user@ip-172-31-32-113.us-west-2.compute.internal>
2020-11-08 13:40:39 +0000

    goal✖編集中２

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-before_action :authenticate_user!  
+before_action :authenticate_user!
+before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = Task.todo.order(updated_at: :desc).page(params[:page])
