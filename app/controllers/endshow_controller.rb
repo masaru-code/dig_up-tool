e6293e26a8e3932a5a16f9da67cb8740d7c2cb59
masaru-code <ppurok@yahoo.co.jp>
2020-11-30 21:14:53 +0900

    doneの工程ページ表示作成途中

--- app/controllers/endshow_controller.rb
+++ app/controllers/endshow_controller.rb
@@ -1,7 +1,12 @@
 class EndshowController < ApplicationController
     before_action :authenticate_user!
 
-    def index
+    def index      
       @users = params[:name].present? ? User.where('name LIKE ?', "%#{params[:name]}%") : User.none
+    end
+
+    def show
+      @user = params[:user_id].present? ?
+      @tasks = @user.tasks.done.order(updated_at: :desc).page(params[:page])
     end
 end
