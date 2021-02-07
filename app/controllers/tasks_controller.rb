55fc264e0df1451688be067e42f00026e0e8f42f
masaru-code <ppurok@yahoo.co.jp>
2021-02-07 09:15:03 +0900

    task/showコントローラーの書き方

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
   before_action :check_ten_todo, only: [:create]
   before_action :set_task, only: [:update]
-  before_action :set_user, only: %i(index show)
+  before_action :set_user, only: %i(index)
   
   def index
     @user = params[:user_id].present? ? @user : current_user
