44417d3f79d6378b65c138bc02e2c3be7282d3f1
masaru-code <ppurok@yahoo.co.jp>
2021-03-14 13:41:59 +0900

    rails 6差分

--- config/application.rb
+++ config/application.rb
@@ -8,18 +8,12 @@ Bundler.require(*Rails.groups)
 
 module DigUp
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
-   config.to_prepare do
-     Devise::SessionsController.layout "devise"
-     Devise::RegistrationsController.layout "devise"
-     Devise::ConfirmationsController.layout "devise"
-     Devise::UnlocksController.layout "devise"
-     Devise::PasswordsController.layout "devise"
-     HomeController.layout "home"
-   end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
